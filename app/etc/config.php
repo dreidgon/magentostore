5e00d36c8b5113cb3d0c1bc8b0d8e3edbac45eb7
dan <dan@example.com>
2022-11-01 11:37:23 +0000

    Added MegaModule_Tutorial module

--- app/etc/config.php
+++ app/etc/config.php
@@ -344,6 +344,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'MegaModule_Tutorial' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1
